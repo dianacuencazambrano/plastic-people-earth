d093773ba8e617f534906384c352bfedb8233268
Diana <diana.cuenca.zambrano@udla.edu.ec>
2023-02-06 23:13:06 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,153 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+import tips from "./helpers/tips.js";
+
+const wins = ref(0)
+const draws = ref(0)
+const losses = ref(0)
+
+const choice = ref(null)
+const computerChoice = ref(null)
+const verdict = ref(null)
+
+const tipChoice = ref(null)
+const tipsChoices = tips
+
+const outcomes = {
+	'plastic': {
+		'plastic': 'draw',
+		'people': 'loss',
+		'tierra': 'win'
+	},
+	'people': {
+		'plastic': 'win',
+		'people': 'draw',
+		'tierra': 'loss'
+	},
+	'earth': {
+		'plastic': 'loss',
+		'people': 'win',
+		'earth': 'draw'
+	}
+}
+
+const play = (c) => {
+	choice.value = c
+	
+	const choices = ['plastic', 'people', 'earth']
+	const random = Math.floor(Math.random() * choices.length)
+	computerChoice.value = choices[random]
+
+	const randomTips = Math.floor(Math.random() * tipsChoices.length)
+	tipChoice.value = tipsChoices[randomTips]
+
+	const outcome = outcomes[choice.value][computerChoice.value]
+	
+	if (outcome === 'win') {
+		wins.value++
+		verdict.value = 'You win!'
+	} else if (outcome === 'loss') {
+		losses.value++
+		verdict.value = 'You Lose!'
+	} else {
+		draws.value++
+		verdict.value = 'It\'s a draw'
+	}
+
+	SaveGame()
+}
+
+const winPercentage = computed(() => {
+	const total = wins.value + draws.value + losses.value
+	return total ? (wins.value / total) * 100 : 0
+})
+
+const SaveGame = () => {
+	localStorage.setItem('wins', wins.value)
+	localStorage.setItem('draws', draws.value)
+	localStorage.setItem('losses', losses.value)
+	localStorage.setItem('tips', tipChoice.value)
+}
+
+const LoadGame = () => {
+	wins.value = parseInt(localStorage.getItem('wins')) || 0
+	draws.value = parseInt(localStorage.getItem('draws')) || 0
+	losses.value = parseInt(localStorage.getItem('losses')) || 0
+	tipChoice.value = parseInt(localStorage.getItem('tips')) || ''
+}
+
+const ResetRound = () => {
+	choice.value = null
+	computerChoice.value = null
+	verdict.value = null
+}
+
+onMounted(() => {
+	LoadGame()
+
+	window.addEventListener('keypress', (e) => {
+		if (e.key === 'r') {
+			ResetRound()
+		}
+	})
+})
+</script>
+
+<template>
+	<div class="bg-gray-700 text-white text-center min-h-screen flex flex-col">
+		<header class="container mx-auto p-6">
+			<h1 class="text-4xl font-bold">Plastic, People, Earth!</h1>
+		</header>
+
+		<main class="container mx-auto p-6 flex-1">
+			<div v-if="choice === null" class="flex items-center justify-center -mx-6">
+
+				<button @click="play('plastic')"
+					class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-pink-500">
+					<img src="./assets/plastic.svg" alt="Rock" class="w-full" />
+				</button>
+
+				<button @click="play('people')"
+					class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-green-500">
+					<img src="./assets/employees.svg" alt="Paper" />
+				</button>
+
+				<button @click="play('earth')"
+					class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-yellow-500">
+					<img src="./assets/worldwide.svg" alt="Scissors" />
+				</button>
+
+			</div>
+
+			<div v-else>
+				<div class="text-3xl mb-4">
+					You picked <span class="text-pink-500">{{ choice }}</span>
+				</div>
+				<div class="text-3xl mb-4">
+					The computer picked <span class="text-green-500">{{ computerChoice }}</span>
+				</div>
+				<div class="text-6xl mb-12">
+					{{ verdict }}
+				</div>
+
+				<button @click="ResetRound" class="bg-pink-500 text-lg py-2 px-4">Reset</button>
+			</div>
+
+			<div class="mt-12 text-3xl mb-4">{{ wins }} : {{ draws }} : {{ losses }}</div>
+
+			<div class="text-lg">Win rate: {{ Math.round(winPercentage) }}%</div>
+			<br>
+			<div class="text-lg"><b>{{ tipChoice }}</b></div>
+		</main>
+
+		<footer class="container mx-auto p-6">
+			<a href="https://www.vecteezy.com/free-vector/rock-paper-scissors-game">Rock Paper Scissors Game Vectors by
+				Vecteezy</a>
+
+				<p> Diana Cuenca Adaptation</p>
+				<p>Recycling tips from:<a href="https://www.recyclingbins.co.uk/recycling-tips/"> https://www.recyclingbins.co.uk/recycling-tips/</a> </p>
+		</footer>
+	</div>
+</template>
+
+<style></style>
---
+++ src/helpers/tips.js
@@ -0,0 +1,17 @@
+const tips = [
+    'Have a system: keep a recycling bag, box, or bin next to your rubbish bin as a reminder for everyone in your house to recycle.', 
+	'Flatten cardboard boxes: you can fit more into your recycling box or bin that way.', 
+	'Recycle plastic bottles: don’t forget items like shampoo and mouthwash bottles. Most councils will accept most types of plastic in recycling, though if in doubt, the label on the bottle should tell you it it’s recyclable.',
+    'Recycle all paper and cardboard: it’s not just newspapers that you can recycle; envelopes, wrapping paper, birthday cards (as long as they don’t contain glitter) and phone books can all be recycled. All types of cardboard can be recycled, so don’t forget the toilet roll tubes and drinks cartons.',
+    'Most metals can be recycled: items like aerosols and kitchen foil can be recycled, as well as your tins and cans.',
+    'Keep a carrier bag in the hallway: this way, you can collect any junk mail and put it in the recycling.',
+    'Make a point of going to a recycling point: if you do this on the way to work, or the supermarket, recycling becomes a part of your routine.',
+    'If you aren’t sure what you can recycle, look it up: check your local council’s website for advice and information on what you can and can’t recycle.',
+    'Take your own bags when you go shopping: this means you avoid using plastic bags unnecessarily and paying the charge.',
+    'Plan your meals and make a list before going shopping: this will help you to avoid overbuying and buying things you don’t need.',
+    'Buy refills: buying refills of detergents, coffee, and hand wash is more environmentally friendly and it’s usually cheaper.',
+    'Avoid excess packaging: packaging adds cost to goods, and excess packaging is wasteful and not good for the environment. Look for unpackaged products or buy products that are packaged in recyclable material.',
+    'Avoid disposable items: buy rechargeable batteries, low energy light bulbs, and razors with interchangeable heads.'
+]
+
+export default tips ;
